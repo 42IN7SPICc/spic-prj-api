b1ea98fab9b47068c6986c5444dbcf851cf611ba
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-19 12:51:21 +0200

    Update GameObject_templates.hpp

--- GameObject_templates.hpp
+++ GameObject_templates.hpp
@@ -1,1 +1,74 @@
 #include "GameObject.hpp"
+
+template<class T>
+std::shared_ptr<GameObject> spic::GameObject::FindObjectOfType(bool includeInactive = false) {
+    for (std::shared_ptr<GameObject> gameObject: All(includeInactive))
+    {
+        try
+        {
+            auto objectPtr = std::dynamic_pointer_cast<T>(gameObject);
+            if (objectPtr) return objectPtr;
+        }
+        catch (const std::bad_cast&)
+        {
+            // do nothing
+        }
+    }
+
+    return nullptr;
+}
+
+template<class T>
+std::vector<std::shared_ptr<GameObject>> spic::GameObject::FindObjectsOfType(bool includeInactive = false) {
+    std::vector<std::shared_ptr<T>> typeObjects;
+
+    for (std::shared_ptr<GameObject> gameObject: All(includeInactive))
+    {
+        try
+        {
+            auto objectPtr = std::dynamic_pointer_cast<T>(gameObject);
+            if(objectPtr) typeObjects.insert(objectPtr);
+        }
+        catch (const std::bad_cast&)
+        {
+            // do nothing
+        }
+    }
+
+    return typeObjects;
+}
+
+template<class T>
+void spic::GameObject::AddComponent(std::shared_ptr<Component> component) {
+
+}
+
+template<class T>
+std::shared_ptr<Component> spic::GameObject::GetComponent() const {
+
+}
+
+template<class T>
+std::shared_ptr<Component> spic::GameObject::GetComponentInChildren() const {
+
+}
+
+template<class T>
+std::shared_ptr<Component> spic::GameObject::GetComponentInParent() const {
+
+}
+
+template<class T>
+std::vector<std::shared_ptr<Component>> spic::GameObject::GetComponents() const {
+
+}
+
+template<class T>
+std::vector<std::shared_ptr<Component>> spic::GameObject::GetComponentsInChildren() const {
+
+}
+
+template<class T>
+std::vector<std::shared_ptr<Component>> spic::GameObject::GetComponentsInParent() const {
+
+}
