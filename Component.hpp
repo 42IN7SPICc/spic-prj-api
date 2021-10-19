cdbf4e778ac4c6fdf03f8a5e7b7459379c68214e
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-19 14:43:50 +0200

    Update

--- Component.hpp
+++ Component.hpp
@@ -19,6 +19,8 @@ namespace spic {
              */
             void Active(bool flag) { active = flag; }
 
+            virtual ~Component() = 0;
+
         private:
             /**
              * @brief Active status.
