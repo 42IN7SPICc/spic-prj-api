bbb46020673c84f3a43585d6f4ee1af47b993d4f
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-19 14:07:11 +0200

    Update GameObject_templates.hpp

--- GameObject_templates.hpp
+++ GameObject_templates.hpp
@@ -15,7 +15,7 @@ std::shared_ptr<spic::GameObject> spic::GameObject::FindObjectOfType(bool includ
         }
     }
 
-    return nullptr;
+    return std::shared_ptr<T>{};
 }
 
 template<class T>
@@ -40,35 +40,79 @@ std::vector<std::shared_ptr<spic::GameObject>> spic::GameObject::FindObjectsOfTy
 
 template<class T>
 void spic::GameObject::AddComponent(std::shared_ptr<spic::Component> component) {
-
+    children.push_back(component);
 }
 
 template<class T>
 std::shared_ptr<spic::Component> spic::GameObject::GetComponent() const {
+    for (std::shared_ptr<spic::Component> component: components)
+    {
+        try
+        {
+            auto componentPtr = std::dynamic_pointer_cast<T>(component);
+            if (componentPtr) return componentPtr;
+        }
+        catch (const std::bad_cast&)
+        {
+            // do nothing
+        }
+    }
 
-}
-
-template<class T>
-std::shared_ptr<spic::Component> spic::GameObject::GetComponentInChildren() const {
-
-}
-
-template<class T>
-std::shared_ptr<spic::Component> spic::GameObject::GetComponentInParent() const {
-
+    return std::shared_ptr<T>{};
 }
 
 template<class T>
 std::vector<std::shared_ptr<spic::Component>> spic::GameObject::GetComponents() const {
+    std::vector<std::shared_ptr<T>> typeComponents;
 
+    for (std::shared_ptr<spic::Component> component: components)
+    {
+        try
+        {
+            auto componentPtr = std::dynamic_pointer_cast<T>(component);
+            if (componentPtr) typeComponents.insert(componentPtr);
+        }
+        catch (const std::bad_cast&)
+        {
+            // do nothing
+        }
+    }
+
+    return typeComponents;
+}
+
+template<class T>
+std::shared_ptr<spic::Component> spic::GameObject::GetComponentInChildren() const {
+    for (const std::shared_ptr<spic::GameObject>& child: children)
+    {
+        if (child) return child->GetComponent<T>();
+    }
+
+    return std::shared_ptr<T>{};
+}
+
+template<class T>
+std::shared_ptr<spic::Component> spic::GameObject::GetComponentInParent() const {
+    return parent ? parent->GetComponent<T>() : std::shared_ptr<T>{};
 }
 
 template<class T>
 std::vector<std::shared_ptr<spic::Component>> spic::GameObject::GetComponentsInChildren() const {
+    std::vector<std::shared_ptr<T>> childComponents;
 
+    for (const std::shared_ptr<spic::GameObject>& child: children)
+    {
+        if (child)
+        {
+            auto components = child->GetComponents<T>();
+            childComponents.insert(childComponents.end(), components.begin(), components.end());
+        }
+    }
+
+    return childComponents;
 }
 
 template<class T>
 std::vector<std::shared_ptr<spic::Component>> spic::GameObject::GetComponentsInParent() const {
-
+    return parent ? parent->GetComponents<T>() : std::vector<std::shared_ptr<T>>{};
 }
