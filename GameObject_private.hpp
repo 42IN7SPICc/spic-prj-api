193ce2a400d99081a6ff1d9af23d5d65c88ffaf0
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-19 14:05:43 +0200

    Update GameObject_private.hpp

--- GameObject_private.hpp
+++ GameObject_private.hpp
@@ -1,4 +1,6 @@
 std::shared_ptr<GameObject> parent;
 std::vector<std::shared_ptr<GameObject>> children;
 
+std::vector<std::shared_ptr<Component>> components;
+
 static std::vector<std::shared_ptr<GameObject>> All(bool includeInactive = false);
