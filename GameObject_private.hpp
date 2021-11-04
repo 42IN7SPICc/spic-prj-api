eb0ea96b73d855edd6c06aa7447ce344f166521b
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-04 11:59:32 +0100

    Move GameObject::All

--- GameObject_private.hpp
+++ GameObject_private.hpp
@@ -2,6 +2,3 @@ std::weak_ptr<GameObject> parent;
 std::vector<std::shared_ptr<GameObject>> children;
 
 std::vector<std::shared_ptr<Component>> components;
-
-public:
-static std::vector<std::shared_ptr<GameObject>> All(bool includeInactive = false);
