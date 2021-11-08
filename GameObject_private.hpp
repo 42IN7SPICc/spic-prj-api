f7e850c58a486a11b156e2cc7d753b697f92a2b4
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-08 16:10:42 +0100

    Renamed some fields and added transform

--- GameObject_private.hpp
+++ GameObject_private.hpp
@@ -1,4 +1,4 @@
-std::weak_ptr<GameObject> parent;
-std::vector<std::shared_ptr<GameObject>> children;
-
-std::vector<std::shared_ptr<Component>> components;
+std::weak_ptr<GameObject> _parent;
+std::vector<std::shared_ptr<GameObject>> _children;
+std::vector<std::shared_ptr<Component>> _components;
+spic::Transform& _transform;
