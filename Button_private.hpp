d39e8315915299b19724c530e7ed9607ac1082da
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-09 11:43:20 +0100

    Update Button_private.hpp

--- Button_private.hpp
+++ Button_private.hpp
@@ -1,1 +1,3 @@
-std::unique_ptr<spic::IMouseListener> _listener;
+static void DestoryListener(spic::IMouseListener* listener)
+
+std::unique_ptr<spic::IMouseListener, decltype(DestoryListener)> _listener;
