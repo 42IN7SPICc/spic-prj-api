0fb4dc6ec4cadb771953ff3aae94b10e8d07ddbc
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-23 13:20:53 +0100

    Create JsonFacade.h

---
+++ JsonFacade.h
@@ -0,0 +1,15 @@
+#ifndef API_JSONFACADE_H
+#define API_JSONFACADE_H
+
+namespace spic {
+    class JsonFacade {
+    public:
+        template<class T>
+        T Deserialize(const std::string &jsonString);
+
+        template<class T>
+        std::string Deserialize(const T &object);
+    };
+}
+
+#endif
