d162d2ec882680e6b0db0d753cbb0a86cf706730
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-23 13:17:34 +0100

    Revert "Update Component.hpp"
    This reverts commit ec936208c1acde2241658163c1dcb544318a2d95.

--- Component.hpp
+++ Component.hpp
@@ -16,46 +16,44 @@ namespace spic {
      * @spicapi
      */
     class Component {
-    public:
-        /**
-         * @brief Getter for active status.
-         * @return true if active, false otherwise.
-         * @spicapi
-         */
-        bool Active() const { return active; }
+        public:
+            /**
+             * @brief Getter for active status.
+             * @return true if active, false otherwise.
+             * @spicapi
+             */
+            bool Active() const;
 
-        /**
-         * @brief flag New active status.
-         * @spicapi
-         */
-        void Active(bool flag) { active = flag; }
+            /**
+             * @brief flag New active status.
+             * @spicapi
+             */
+            void Active(bool flag);
 
-        virtual ~Component() = default;
+            virtual ~Component() = default;
 
-        /**
-         * @brief Get the GameObject this component belongs to.
-         * @sharedapi
-         */
-        std::weak_ptr<spic::GameObject> GameObject() const;
+            /**
+             * @brief Get the GameObject this component belongs to.
+             * @sharedapi
+             */
+            std::weak_ptr<spic::GameObject> GameObject() const;
 
-        /**
-         * @brief Set the GameObject this component belongs to.
-         * @param gameObject the GameObject this component belongs to.
-         * @sharedapi
-         */
-        void GameObject(std::weak_ptr<spic::GameObject> gameObject);
+            /**
+             * @brief Set the GameObject this component belongs to.
+             * @param gameObject the GameObject this component belongs to.
+             * @sharedapi
+             */
+            void GameObject(std::weak_ptr<spic::GameObject> gameObject);
 
-    private:
-        /**
-         * @brief Active status.
-         * @spicapi
-         */
-        bool active;
+        private:
+            /**
+             * @brief Active status.
+             * @spicapi
+             */
+            bool active;
 
 #if __has_include("Component_private.hpp")
-
 #include "Component_private.hpp"
-
 #endif
     };
 
