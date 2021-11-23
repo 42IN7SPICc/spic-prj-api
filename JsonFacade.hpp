ebe999a7794f5ede16fb4bf9509035de42f2e87c
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-23 13:23:42 +0100

    Update JsonFacade.hpp

--- JsonFacade.hpp
+++ JsonFacade.hpp
@@ -1,14 +1,16 @@
 #ifndef API_JSONFACADE_HPP
 #define API_JSONFACADE_HPP
 
+#include <string>
+
 namespace spic {
     class JsonFacade {
     public:
         template<class T>
-        static T Deserialize(const std::string &jsonString);
+        static T Deserialize(const std::string& jsonString);
 
         template<class T>
-        static std::string Serialize(const T &object);
+        static std::string Serialize(const T& object);
     };
 }
 
